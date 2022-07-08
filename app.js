43a6ba3cacb30b3b588b625d257612c6de8abed9
Mark Jayson Rodelas <markjaysonrodelas.0521@gmail.com>
2022-07-08 19:57:11 +0800

    Adding functions
        //Prevent form from submitting
        //Todo DIV
        //Create LI
        //Checked Mark Button
        //Trash Button

--- app.js
+++ app.js
@@ -4,5 +4,35 @@ const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 
 //Event Listeners
+todoButton.addEventListener('click', addTodo)
+
 
 //Functions
+function addTodo(event){
+    //prevent form from submitting
+    event.preventDefault();
+
+    //Todo DIV
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todo");
+
+    //Create LI
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'hey'
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+    //Checked Mark Button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>'
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+
+    //Trash Button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>'
+    trashButton.classList.add("complete-btn");
+    todoDiv.appendChild(trashButton);   
+    
+
+}
