b6136b18aba26838138e483b91c5a72ac9db988e
MJRodelas <markjaysonrodelas.0521@gmail.com>
2022-07-10 20:12:51 +0800

    Bug found
    it is under the HTML
            <ul class="todo-list">
            </ul>
    
    it should be
            <ul class="todo-list"></ul>

--- app.js
+++ app.js
@@ -79,6 +79,15 @@ function filterTodo(e){
                 else {
                     todo.style.display = "none";
                 }
+                break;
+            case "uncompleted":
+                if (todo.classList.contains("uncompleted")){
+                    todo.style.display = "flex";
+                } 
+                else {
+                    todo.style.display = "none";
+                }
+                break;            
         }
     });
 }
